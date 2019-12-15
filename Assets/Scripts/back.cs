746fd7e33a5d8ccc3f8393a7b49fda06cb2eb5b0
Ahmad Naufal Hakim Lubis <anaufal2031@gmail.com>
2019-12-15 13:11:49 +0700

    Init

---
+++ Assets/Scripts/back.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class back : MonoBehaviour
+{
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+                Application.Quit();
+                return;
+            }
+        }
+    }
+}
+        
+    
